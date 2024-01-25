df63f80d5243fd70162763fa407678f84bea3543
RT109A1 <rt109a5@gmail.com>
2024-01-25 21:16:30 +0300

    Баланс и переключение контекста. Вау.

--- test.py
+++ test.py
@@ -15,13 +15,13 @@ import pickle
 import subprocess
 
 ### Model database ###
-model_gpt35 = 'openai/gpt-3.5-turbo'
-model_gpt4 = 'openai/gpt-4-1106-preview'
-model_noromaid = 'neversleep/noromaid-20b'
-model_openchat = 'openchat/openchat-7b:free'
-model_mythomist = 'gryphe/mythomist-7b:free'
-model_mythomax = 'gryphe/mythomax-l2-13b'
-model_nousllama13 = 'nousresearch/nous-hermes-llama2-13b'
+model_gpt35 = ['openai/gpt-3.5-turbo', 8]
+model_gpt4 = ['openai/gpt-4-1106-preview', 100]
+model_noromaid = ['neversleep/noromaid-20b', 25]
+model_openchat = ['openchat/openchat-7b:free', 1]
+model_mythomist = ['gryphe/mythomist-7b:free', 1]
+model_mythomax = ['gryphe/mythomax-l2-13b', 3]
+model_airoboros = ['jondurbin/airoboros-l2-70b', 5]
 
 
 def getUserFile():
@@ -75,8 +75,10 @@ def sendRequestOpenAI(text, model):
   return completion.choices[0].message.content
 
 
-def sendRequest(text, model, userlist, id):
+def sendRequest(text, model_a, userlist, id):
   user = findUser(id, userlist)
+  model = model_a[0]
+  model_price = model_a[1]
   usercontext = user[4]
   template_user = {'role': 'user', 'content': 'placeholder'}
   newresp = template_user
@@ -86,14 +88,12 @@ def sendRequest(text, model, userlist, id):
   else:
      usercontext.pop(0)
      usercontext.append(newresp)
-  usercontext.append(newresp)
-  print(usercontext)
   response = requests.post(
     url="https://openrouter.ai/api/v1/chat/completions",
     headers={
       "Authorization": f"Bearer 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",
-      "HTTP-Referer": f"telegram.org", # Optional, for including your app on openrouter.ai rankings.
-      "X-Title": f"AS1MOV", # Optional. Shows in rankings on openrouter.ai.
+      "HTTP-Referer": f"telegram.org",
+      "X-Title": f"AS1MOV",
     },
     data = json.dumps({
       "model": model,
@@ -101,11 +101,17 @@ def sendRequest(text, model, userlist, id):
     })
   )
   resp_dict = response.json()
+  print(resp_dict)
   op = 'RESPONSE: '
   temptext = "https://openrouter.ai/api/v1/generation?id="
   temptext += resp_dict["id"]
   a1 = requests.get(url=temptext, headers={'Authorization': f'Bearer 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'})
   a2 = a1.json()
+  a3 = a2['data']
+  a4 = a3['native_tokens_prompt']
+  a41 = a3['native_tokens_completion']
+  tokens_used = a4 + a41
+  credits_used = tokens_used * model_price
   rp1 = resp_dict["choices"]
   rp2 = rp1[0]
   rp3 = rp2["message"]
@@ -114,22 +120,28 @@ def sendRequest(text, model, userlist, id):
   else:
      usercontext.pop(0)
      usercontext.append(rp3)
-  print(usercontext)
+  usercred = user[1]
+  usercred -= credits_used
+  user[1] = usercred
+  print(user)
   userlist = closeAndOpenUserFile(userlist)
   rp4 = rp3["content"]
   op += rp4
-  return op, userlist
+  return op, userlist, tokens_used
 
-def sendRequestNoContext(text, model):
+def sendRequestNoContext(text, model_a, userlist, id):
+  user = findUser(id, userlist)
+  model = model_a[0]
+  model_price = model_a[1]
   response = requests.post(
     url="https://openrouter.ai/api/v1/chat/completions",
     headers={
       "Authorization": f"Bearer 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",
-      "HTTP-Referer": f"telegram.org", # Optional, for including your app on openrouter.ai rankings.
-      "X-Title": f"AS1MOV", # Optional. Shows in rankings on openrouter.ai.
+      "HTTP-Referer": f"telegram.org",
+      "X-Title": f"AS1MOV",
     },
     data = json.dumps({
-      "model": model, # Optional
+      "model": model,
       "messages": [
         {"role": "user", "content": text}
       ]
@@ -141,12 +153,23 @@ def sendRequestNoContext(text, model):
   temptext += resp_dict["id"]
   a1 = requests.get(url=temptext, headers={'Authorization': f'Bearer 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'})
   a2 = a1.json()
+  a3 = a2['data']
+  a4 = a3['native_tokens_prompt']
+  a41 = a3['native_tokens_completion']
+  tokens_used = a4 + a41
+  credits_used = tokens_used * model_price
   rp1 = resp_dict["choices"]
   rp2 = rp1[0]
   rp3 = rp2["message"]
   rp4 = rp3["content"]
   op += rp4
-  return op
+  usercred = user[1]
+  usercred -= credits_used
+  user[1] = usercred
+  print(user)
+  userlist = closeAndOpenUserFile(userlist)
+  op += rp4
+  return op, userlist, tokens_used
 
 bot = Bot(token="6925589253:AAHucNlyVfHxJ5ErNqU-krI2cqeRmt0OGxo")
 dp = Dispatcher()
@@ -158,30 +181,67 @@ async def mainFunction(message: Message):
     user = findUser(userid, all_user_list)
     # print(user)
     if user is not False:
-      usercontext = user[3]
-      usermodel = user[2]
-      usercredits = user[1]
-      usermsgs = user[4]
-      if message.text != '/start':
-          if usercontext is False:
-            ans = sendRequestNoContext(message.text, usermodel)
-            await message.answer(ans)
-            print(str(userid) + ':')
-            print(message.text)
-            print(ans)
-            print('--------------------')
-          else:
-            ans, all_user_list = sendRequest(message.text, usermodel, all_user_list, userid)
-            await message.answer(ans)
-            print(str(userid) + ':')
-            print(message.text)
-            print(ans)
-            print('--------------------')
+      if message.text != '/balance':
+        usercontext = user[3]
+        usermodel = user[2]
+        usercredits = user[1]
+        usermsgs = user[4]
+        if message.text == '/start':
+            await message.answer(
+               """Привет и добро пожаловать в бота AS1MOV! Полезные команды:
+               /help -- помощь
+               /settings -- настройки
+               /balance -- проверить баланс"""
+                                 )
+        elif message.text == '/help':
+           await message.answer(
+               """Полезные команды:
+               /help -- помощь
+               /settings -- настройки
+               /balance -- проверить баланс
+               По всем вопросам обращайтесь к создателю: @Might_Is_the_Right"""
+                                 )
+        elif message.text == '/settings':
+           await message.answer('В процессе разработки. Если хотите отключить контекст, напишите /context.')
+        elif message.text == '/context':
+           if usercontext is False:
+              usercontext = True
+              user[3] = usercontext
+              await message.answer('Контекст включён. Затраты кредитов увеличены.')
+              all_user_list = closeAndOpenUserFile(all_user_list)
+           else:
+              usercontext = False
+              user[3] = usercontext
+              await message.answer('Контекст отключён.')
+              all_user_list = closeAndOpenUserFile(all_user_list)
+        else:
+            if usercontext is False:
+              ans, all_user_list, tkns = sendRequestNoContext(message.text, usermodel, all_user_list, userid)
+              await message.answer(ans)
+              sp = 'Токенов всего: ' + str(tkns)
+              await message.answer(sp)
+              print(str(userid) + ':')
+              print(message.text)
+              print(ans)
+              print('--------------------')
+            else:
+              ans, all_user_list, tkns = sendRequest(message.text, usermodel, all_user_list, userid)
+              await message.answer(ans)
+              sp = 'Токенов всего: ' + str(tkns)
+              await message.answer(sp)
+              print(str(userid) + ':')
+              print(message.text)
+              print(ans)
+              print('--------------------')
       else:
-          await message.answer('Error.')
+         usercredits = user[1]
+         msg = 'У вас осталось '
+         msg += str(usercredits)
+         msg += ' кредитов.'
+         await message.answer(msg)
     else:
        await message.answer("Ошибка. Подождите, пока происходит ваша регистрация...")
-       newuser = [userid, 50000, model_gpt35, True, [{"role": "user", "content": "This is a placeholder message. You can ignore it."}]]
+       newuser = [userid, 50000, model_openchat, True, [{"role": "user", "content": "This is a placeholder message. You can ignore it."}]]
        all_user_list.append(newuser)
        all_user_list = closeAndOpenUserFile(all_user_list)
        await message.answer("Готово!")
